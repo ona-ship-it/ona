2afedfc81277c5f9c5325aab641223e560d5a23f
ona-ship-it <samiraeddaoudi88@gmail.com>
2026-03-07 00:39:37 +0000

    Update route.ts

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,7 +1,6 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
 import { type NextRequest, NextResponse } from 'next/server'
-import { createClient } from '@/lib/supabaseServer' // Use that server client we made!
-
+import { createClient } from '../../../lib/supabaseServer'
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const token_hash = searchParams.get('token_hash')
